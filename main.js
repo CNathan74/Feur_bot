221c4e16835510373a172257d641ef130ee7a0b5
Nathan <nathan.charvin@gmail.com>
2021-02-26 00:23:57 +0100

    Add

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
 
 //Toutes les actions à faire quand le bot se connecte
 client.on("ready", function () {
@@ -11,7 +14,31 @@ client.on("ready", function () {
 client.on("message", function (message) {
     let msg = message.content.toLowerCase()
     if ((msg.endsWith("quoi") === true) || (msg.endsWith("quoi ?") === true) || (msg.endsWith("quoi?") === true)) {
-        message.channel.send("Feur")
+        message.react('🇫')
+        message.react('🇪')
+        message.react('🇺')
+        message.react('🇷')
+        //message.channel.send("Feur")
+    }
+    if ((msg.endsWith("oui") === true) || (msg.endsWith("oiu.") === true) || (msg.endsWith("oui !") === true) || (msg.endsWith("oui ?") === true)) {
+        message.channel.send("Stiti")
+    }
+    if ((msg.endsWith("ah") === true) || (msg.endsWith("ah.") === true) || (msg.endsWith("ah !") === true) || (msg.endsWith("ah!") === true)) {
+        //message.channel.send("Stérix")
+        message.react('🇸')
+        message.react('🇹')
+        message.react('🇪')
+        message.react('🇷')
+        message.react('🇮')
+        message.react('🇽')
+    }
+    if ((msg.endsWith("oh") === true) || (msg.endsWith("oh.") === true) || (msg.endsWith("oh !") === true) || (msg.endsWith("oh!") === true)) {
+        //message.channel.send("Bélix")
+        message.react('🇧')
+        message.react('🇪')
+        message.react('🇱')
+        message.react('🇮')
+        message.react('🇽')
     }
 })
 
